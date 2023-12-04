d3369631bb13d75b4b46bf98b2836ba816e7a798
Chanuka <chanuka.weerakkody123@gmail.com>
2023-12-04 12:10:31 +0530

    add route to navigate

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ require('dotenv').config();
 var express = require("express");
 exports.app = express();
 var cors = require("cors");
-//import cookieParser from "cookie-parser";
 //body parser
 exports.app.use(express.json({ limit: "50mb" }));
 //cookie-parser
@@ -16,3 +15,8 @@ exports.app.use(cors({
     origin: process.env.ORIGIN
 }));
 //testing api
+exports.app.get("/test", function (req, res, next) {
+    res.status(200).json({
+        message: "Test route successful",
+    });
+});
--- app.ts
+++ app.ts
@@ -1,10 +1,11 @@
 import cookieParser = require("cookie-parser");
-
+import { Request, Response, NextFunction } from "express";
 require('dotenv').config();
 import * as express from "express";
 export const app = express();
 
 import * as cors from "cors";
+
 
 //body parser
 app.use(express.json({limit: "50mb"}));
@@ -18,9 +19,18 @@ app.use(cors({
 }));
 
 //testing api
+app.get("/test", (req: Request, res: Response, next: NextFunction) => {
+    res.status(200).json({
+        message: "Test route successful",
+    });
+});
 
-
-
+//unknown route
+app.all("*",(req:Request, res:Response, next:NextFunction) =>{
+   const err = new Error(`Route ${req.originalUrl} not found`)as any;
+   err.statusCode =404;
+   next(err);
+});
 
 
 
