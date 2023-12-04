393756cf39b71fc2d2ad4bc9f5b7b6f2b58212c1
Chanuka <chanuka.weerakkody123@gmail.com>
2023-12-04 13:05:19 +0530

    create mongodb connection successfully

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var app_1 = require("./app");
+var db_1 = require("./util/db");
 require("dotenv").config();
 //create server
 app_1.app.listen(process.env.PORT, function () {
     console.log("server is connected with ".concat(process.env.PORT));
+    (0, db_1.default)();
 });
--- server.ts
+++ server.ts
@@ -1,8 +1,10 @@
 import { app } from "./app";
+import connectDB from "./util/db";
 require("dotenv").config();
 
 
 //create server
 app.listen(process.env.PORT, () =>{
     console.log(`server is connected with ${process.env.PORT}`);
+    connectDB();
 });
