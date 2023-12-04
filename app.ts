3769eb4945d80a1072a61bce28506abdb3153187
Chanuka <chanuka.weerakkody123@gmail.com>
2023-12-04 11:47:29 +0530

    create body parser limit, cookie parser and cors policy

--- app.ts
+++ app.ts
@@ -1,2 +1,24 @@
+require('dotenv').config();
 import * as express from "express";
 export const app = express();
+
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+//body parser
+app.use(express.json({limit: "50mb"}));
+
+//cookie-parser
+app.use(cookieParser());
+
+//cors => cross origin resource sharing
+app.use(cors({
+    origin: process.env.ORIGIN
+}));
+
+
+
+
+
+
+
