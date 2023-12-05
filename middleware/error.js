c04de347ecb0f78c9e8835eb3efabe0ebce9af7d
Chanuka <chanuka.weerakkody123@gmail.com>
2023-12-05 14:31:38 +0530

    create middleware directory and error.ts class to handle defined errors

---
+++ middleware/error.js
@@ -0,0 +1,32 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var ErrorHandler_1 = require("../util/ErrorHandler");
+module.exports = function (err, req, res, next) {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal server error";
+    //wrong mongo id error
+    if (err.name === 'castError') {
+        var message = "Resource not found. Invalid: ".concat(err.path);
+        err = new ErrorHandler_1.default(message, 400);
+    }
+    //Duplicate key error
+    if (err.code === 11000) {
+        var message = "Duplicate ".concat(Object.keys(err.keyValue), " entered");
+        err = new ErrorHandler_1.default(message, 400);
+    }
+    //Wrong jwt error
+    if (err.name === 'JsonWebTokenError') {
+        var message = "Json web token is invalid. Try again";
+        err = new ErrorHandler_1.default(message, 400);
+    }
+    //jwt expire error
+    if (err.name === 'TokenExpiredError') {
+        var message = "Json web token is expired. Try again";
+        err = new ErrorHandler_1.default(message, 400);
+    }
+    /*res.status(err.statusCode as number).json({
+        success: false,
+        message: err.message
+    })
+*/
+};
--- util/ErrorHandler.js
+++ util/ErrorHandler.js
@@ -1,3 +1,4 @@
+"use strict";
 var __extends = (this && this.__extends) || (function () {
     var extendStatics = function (d, b) {
         extendStatics = Object.setPrototypeOf ||
@@ -13,6 +14,7 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+Object.defineProperty(exports, "__esModule", { value: true });
 var ErrorHandler = /** @class */ (function (_super) {
     __extends(ErrorHandler, _super);
     function ErrorHandler(message, statusCode) {
@@ -23,4 +25,4 @@ var ErrorHandler = /** @class */ (function (_super) {
     }
     return ErrorHandler;
 }(Error));
-module.exports = ErrorHandler;
+exports.default = ErrorHandler;
