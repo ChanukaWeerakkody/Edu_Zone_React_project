b4dbf7016a8d514dc60f7a0e821144eef332c108
Chanuka <chanuka.weerakkody123@gmail.com>
2023-12-05 14:13:12 +0530

    create ErrorHandling class successfully

--- util/ErrorHandler.ts
+++ util/ErrorHandler.ts
@@ -1,3 +1,9 @@
 class ErrorHandler extends Error{
-
+    statusCode:number;
+    constructor(message:any,statusCode:number){
+        super(message);
+        this.statusCode = statusCode;
+        Error.captureStackTrace(this,this.constructor);
+    }
 }
+module.exports=ErrorHandler;
