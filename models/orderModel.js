315c168ff5d0a095faa7e03fd53a3bf19799c75d
Chanuka <chanuka.weerakkody123@gmail.com>
2023-12-26 10:54:02 +0530

    create order model

---
+++ models/orderModel.js
@@ -0,0 +1,16 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = require("mongoose");
+var orderSchema = new mongoose_1.Schema({
+    courseId: {
+        type: String,
+        required: true
+    }, userId: {
+        type: String,
+        required: true
+    },
+    payment_info: {
+        type: Object,
+        //required:true
+    }
+}, { timestamps: true });
